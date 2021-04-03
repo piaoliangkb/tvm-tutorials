119cff8c01569c205f69fc905d40e20fec204684
piaoliangkb <piaoliangkb@gmail.com>
2021-04-03 21:21:45 +0800

    Code format for load-and-run-onnx.py

--- tvmc-tutorial/load-and-run-onnx.py
+++ tvmc-tutorial/load-and-run-onnx.py
@@ -1,11 +1,12 @@
-import onnx
-import onnxruntime
-import numpy as np
-from tvm.contrib.download import download_testdata
-from scipy.special import softmax
 import time
 
-# ref: 
+import numpy as np
+import onnx
+import onnxruntime
+from scipy.special import softmax
+from tvm.contrib.download import download_testdata
+
+# ref:
 # https://github.com/Azure/MachineLearningNotebooks/blob/master/how-to-use-azureml/deployment/onnx/onnx-modelzoo-aml-deploy-resnet50.ipynb
 # https://pytorch.org/docs/stable/onnx.html
 # https://pytorch.org/tutorials/advanced/super_resolution_with_onnxruntime.html
@@ -29,7 +30,7 @@ labels_url = "https://s3.amazonaws.com/onnx-model-zoo/synset.txt"
 labels_path = download_testdata(labels_url, "synset.txt", module="data")
 
 with open(labels_path, "r") as f:
-    labels = [l.rstrip() for l in f]
+    labels = [line.rstrip() for line in f]
 
 scores = softmax(outputs)
 print(type(scores))
@@ -38,4 +39,4 @@ scores = np.squeeze(scores)
 ranks = np.argsort(scores)[::-1]
 
 for rank in ranks[0:5]:
-    print(f"class={labels[rank]} with probability={scores[rank]}") 
+    print(f"class={labels[rank]} with probability={scores[rank]}")
